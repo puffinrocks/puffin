6dfe4afec878c46109bb2a662d8e3b805001b82c
Jarek Lipski <loomchild@loomchild.net>
2015-12-02 15:35:55 +0100

    Configurable remote machine

--- puffin/core/machine.py
+++ puffin/core/machine.py
@@ -8,20 +8,15 @@ def init():
     pass
 
 def get_machine():
-    if app.config["MACHINE"] == "remote":
-        return get_remote_machine()
-    else:
-        return get_local_machine()
+    url = app.config["MACHINE_URL"]
+    path = app.config["MACHINE_PATH"]
 
-def get_local_machine():
-    return Machine('unix://var/run/docker.sock')
+    tls_config = None
+    if path:
+        tls_config = TLSConfig(
+            client_cert=(path + 'cert.pem', path + 'key.pem'),
+            verify=path + 'ca.pem'
+        )
 
-def get_remote_machine():
-    return Machine("https://52.29.5.191:2376", 
-        TLSConfig(
-            client_cert=(
-                '/home/loomchild/.docker/machine/machines/frankfurt/cert.pem', 
-                '/home/loomchild/.docker/machine/machines/frankfurt/key.pem'),
-            verify='/home/loomchild/.docker/machine/machines/frankfurt/ca.pem'
-        ))
+    return Machine(url, tls_config)
 
