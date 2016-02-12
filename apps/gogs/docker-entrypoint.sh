8c651da18d116c5d65c46c1e0ab0e3ae156c264c
Jarek Lipski <loomchild@loomchild.net>
2016-02-12 14:18:14 +0100

    Fix Gogs entrypoint

--- apps/gogs/docker-entrypoint.sh
+++ apps/gogs/docker-entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-# Don't change secret on every retart
+# Don't change secret on every restart
 if [[ ! -f /data/gogs/conf/app.ini ]]; then
     mkdir -p /data/gogs/conf
-    mkdir -p /data/gogs/log
-    mkdir -p /data/gogs/data
-    mkdir -p /data/ssh
     SECRET=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`
     sed "s/\$SECRET/$SECRET/" /app/gogs/app.ini >  /data/gogs/conf/app.ini
 fi
 
 sed -i "s/\$DOMAIN/$VIRTUAL_HOST/" /data/gogs/conf/app.ini
 
-chown -R git /data/
+chown -R git /data/gogs/conf
 
-/bin/s6-svscan /app/gogs/docker/s6/
+docker/start.sh $@
