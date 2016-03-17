96816ffa8d6f752f5e774adbb28c9e115b80c390
Jarek Lipski <loomchild@loomchild.net>
2016-03-18 00:34:45 +0100

    DNS app

--- apps/_dns/docker-entrypoint.sh
+++ apps/_dns/docker-entrypoint.sh
@@ -1,10 +1,8 @@
-#!/usr/local/bin/dumb-init /bin/bash
+#!/bin/bash
 set -e
 
 HOST=`/sbin/ip route|awk '/default/ { print $3 }'`
 
 sed "s/\$HOST/$HOST/" /etc/dnsmasq.conf.template > /etc/dnsmasq.conf
-
-echo "nameserver 127.0.0.1" > /etc/resolv.conf
 
 /usr/sbin/dnsmasq -k
