b49ae63023a177d9e93a0e7e13d150f60e9b418a
Jarek Lipski <loomchild@loomchild.net>
2016-03-20 21:13:13 +0100

    Enable email in Gogs

--- apps/gogs/docker-entrypoint.sh
+++ apps/gogs/docker-entrypoint.sh
@@ -2,14 +2,17 @@
 set -e
 
 # Don't change secret on every restart
-if [[ ! -f /data/gogs/conf/app.ini ]]; then
-    mkdir -p /data/gogs/conf
+if [[ ! -f /data/gogs/conf/secret ]]; then
     SECRET=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12`
-    sed "s/\$SECRET/$SECRET/" /app/gogs/app.ini >  /data/gogs/conf/app.ini
+    echo "SECRET=$SECRET" > /data/gogs/conf/secret
 fi
 
-sed -i "s/\$DOMAIN/$VIRTUAL_HOST/" /data/gogs/conf/app.ini
-
+cp /app/gogs/app.ini  /data/gogs/conf/app.ini
 chown -R git /data/gogs/conf
+
+. /data/gogs/conf/secret
+
+sed -i "s/\$SECRET/$SECRET/" /data/gogs/conf/app.ini
+sed -i "s/\$DOMAIN/$VIRTUAL_HOST/" /data/gogs/conf/app.ini
 
 docker/start.sh $@
