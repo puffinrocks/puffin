af54cd93169ee13a7f2ef984ba3959b67c146756
Jarek Lipski <loomchild@loomchild.net>
2016-03-28 23:45:12 +0200

    Fix Flarum app - restart issue

--- apps/flarum/docker-entrypoint.sh
+++ apps/flarum/docker-entrypoint.sh
@@ -5,11 +5,10 @@ if [ ! -e '/var/www/html/index.php' ]; then
 	tar cf - --one-file-system -C /usr/src/flarum . | tar xf -
     sleep 20
     php flarum install -d
+    mysql -hdb -uflarum -pflarum flarum < /usr/src/flarum/config.sql
 fi
 
 cp /usr/src/flarum/config.php .
 chown -R www-data /var/www/html
-
-mysql -hdb -uflarum -pflarum flarum < /usr/src/flarum/config.sql
 
 exec "$@"
