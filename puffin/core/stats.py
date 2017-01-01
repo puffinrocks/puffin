bc0e7547f0f0774eaf782455ccc70aefab34ef3b
Jarek Lipski <loomchild@loomchild.net>
2017-01-01 17:22:07 +0100

    Apps stats

--- puffin/core/stats.py
+++ puffin/core/stats.py
@@ -18,11 +18,15 @@ def init():
 def get_stats():
     stats = Stats()
     stats.users = get_users()
+    stats.apps = get_apps()
     stats.containers = get_containers()
     return stats    
 
 def get_users():
     return len([u for u in get_all_users() if u.confirmed])
+
+def get_apps():
+    return len(docker.get_all_running_applications())
 
 def get_containers():
     return len(docker.get_containers(docker.get_client()))
