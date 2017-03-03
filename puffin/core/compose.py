fbd99212c7af806137f996ac3c1d6c018f9402a7
Jarek Lipski <loomchild@loomchild.net>
2017-03-04 00:39:52 +0100

    Add Let's Encrypt env var

--- puffin/core/compose.py
+++ puffin/core/compose.py
@@ -22,7 +22,8 @@ def compose_run(machine, user, application, *arguments, **environment):
     args += arguments
 
     domain = get_application_domain(user, application)
-    env = dict(PATH=environ['PATH'], VIRTUAL_HOST=domain)
+    env = dict(PATH=environ['PATH'], VIRTUAL_HOST=domain,
+            LETSENCRYPT_HOST=domain)
     env.update(get_env_vars(machine))
     env.update(**environment)
 
